87d38a7b8862e00dc515825ceca536dfc6e33ae4
Ney <ney@pop-os.localdomain>
2019-11-26 15:37:44 -0400

    Cambie nombre de la clase vectorBitwise

--- Parte_Lógica/VectorBits_Generic_Resize.java
+++ Parte_Lógica/VectorBits_Generic_Resize.java
@@ -4,7 +4,7 @@ package Parte_Lógica;
 import java.util.Scanner;
 import java.lang.*;
 
-public class VectorBitwise_Generico{
+public class VectorBits_Generic_Resize{
   int Vector[];
   int dim;
   int NumberBit;
@@ -23,11 +23,13 @@ public class VectorBitwise_Generico{
   }
   
   private int GetNumero_Of_Elemento(int Position){
+  
     int cantidadDeVector = (((Position - 1) * NumberBit) / 32);
     return cantidadDeVector;
   }
   
   private int GetNumero_Of_Bit(int Position){
+  
     int NumeroDEl_Bit =  (((Position - 1) * NumberBit) % 32);  //////////
     System.out.println("mod=" + String.valueOf(NumeroDEl_Bit));
     return NumeroDEl_Bit;
@@ -35,6 +37,7 @@ public class VectorBitwise_Generico{
   }
   
   public void Insertar(int Elemento, int Position){
+  
     if(Position <= dim){
       Redimensionar(Position);
       dim = Position;
@@ -66,7 +69,7 @@ public class VectorBitwise_Generico{
   }
   
   public int Sacar(int Position){
-  
+    if(Position <= dim){
     int Numero_Vector = GetNumero_Of_Elemento(Position);
     int numberBit = GetNumero_Of_Bit(Position);
     
@@ -87,6 +90,34 @@ public class VectorBitwise_Generico{
       Mask = Mask | Mask1;
     }
     return Mask;
+    }else{
+      System.out.println("numero a sacar fuera de rango");
+      return -1;
+    }
+  }
+  
+  public void Redimensionar(int Position){
+  
+    VectorBits_Generic_Resize DimAux = new VectorBits_Generic_Resize(dim, NumberBit);
+    for(int i = 0; i <= DimAux.Vector.length -1; i++){
+      DimAux.Vector[i] = Vector[i];
+    }
+    
+    int numberBit = Position * NumberBit;
+    int Numero_Vector = numberBit / 32;
+    
+    if(numberBit % 32 != 0){
+      Numero_Vector++;
+    }
+    
+    Vector = new int [Numero_Vector];
+    for(int i = 0; i <= DimAux.Vector.length -1; i++){
+      if(i < DimAux.Vector[i]){
+        DimAux.Vector[i] = Vector[i];
+      }
+      
+    }
+    
   }
   
   public String ObtenerTodo(){
